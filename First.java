aa64d5fb538f4ad2d0a1d1eb48e988ad41847ea7
priyankar909 <priyankarkumar728@gmail.com>
2023-10-27 20:21:42 +0530

    first commit

---
+++ First.java
@@ -0,0 +1,17 @@
+public class First {
+    public static void main(String[] args) {
+             print(2);
+     }
+     static void print(int n){
+          if(n==5){
+               System.out.println(5);
+          return;
+     }
+     System.out.println(n);
+     print(n+1);
+    }
+}
+        
+    
+    
+
